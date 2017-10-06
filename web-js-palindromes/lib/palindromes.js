95d0cdd35288d661d91689e07d11b6919e3c1f02
Shane Richman <Shane.M.Richman@gmail.com>
2017-10-06 19:26:28 +0000

    attached web-js-palindromes to repo

---
+++ web-js-palindromes/lib/palindromes.js
@@ -0,0 +1,6 @@
+function isPalindrome(s) {
+  var stringReverse = s.split("").reverse().join("").replace(/ /g, "");
+  return s.replace(/ /g, "") == stringReverse;
+}
+
+module.exports = isPalindrome;
